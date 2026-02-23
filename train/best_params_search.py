55ec1f8e848453a1b8c637d230b92e0b81ec8597
Chaeil Moon <102636528+chae2@users.noreply.github.com>
2026-02-23 23:48:07 +0900

    Feat: 최적 파라미터 탐색 (#24)
    * feat: test 데이터셋 추가
    
    - 모델 학습 후 테스트 추가
    - 데이터셋 생성 파일에 test 데이터셋 생성 추가
      - 추가적인 이슈 해결: 데이터셋 고정 및 데이터셋 캐싱으로 속도 극대화
    
    * refactor: 최종 모델 평가 확인 소수점자리 수정
    
    * refactor: batch_size 최적화
    
    * refactor: GM 훈련 코드 리팩터링
    
    * feat: gm 학습 모델 사용자 옵션 추가
    
    * doc: test 패키지명 변경
    
    * feat: gm transformer에 fp16 가속 설정 추가
    
    * doc: optuna 설정 및 optimizer 인수 추가
    
    * doc: gloss_transformer 모델명 변경
    
    * feat: 데이터셋 크기 필드화
    
    * fix: 잘못된 경로 수정
    
    * feat: 옵션 추가 (스크립트 자동화 파이프라인 위해)
    
    * refactor: 옵션 출력에 [default] 메시지 추가
    
    * fix: checkpoint 파일 경로
    
    * fix: 데이터셋 생성 시 max_seq 변경
    
    * fix: 필드에 max_len 전달 추가
    
    * doc: requirements-win 업데이트
    
    * fix: 차원수 umap output으로 조정
    
    * fix: dataset이 subset_ratio로 너무 작아지게 되는 경우 방지

---
+++ train/best_params_search.py
@@ -0,0 +1,151 @@
+import evaluate
+import keras
+import keras.optimizers
+
+keras.mixed_precision.set_global_policy("mixed_float16") # fp16 가속 keras3 버전
+import optuna
+import tensorflow as tf
+from optuna.storages import RDBStorage
+
+from load_data.create_dataset import TrainDataLoader
+from src.backbone import get_model
+from src.config import OPTUNA_TRIALS_PATH, LOAD_DATA, EPOCHS, NUM_CLASSES, SUBSET_RATIO, \
+    BEST_PARAMS_PATH, OPTUNA_MODEL, OPTUNA_STUDY_NAME, N_TRIALS, LOCAL_PATHS, UMAP_OUTPUT_DIM
+
+
+def objective(trial):
+    # 1. 하이퍼파라미터 탐색 공간 정의
+    learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-3, log=True)
+    batch_size = trial.suggest_categorical("batch_size", [16, 32, 64])
+    num_train_epochs = trial.suggest_int("num_train_epochs", 30, EPOCHS)
+    weight_decay = trial.suggest_float("weight_decay", 0.0, 0.3)
+    sequence_length = trial.suggest_int("sequence_length", 100, 380)
+
+    # 2. 데이터 불러오기
+    train_dataset, val_dataset, _ = loader.create_transformer_dataset(batch_size=batch_size, max_len=sequence_length)
+    # 2-1. 학습 데이터 줄이기
+    num_train = int(loader.train_size * subset_ratio)
+    train_dataset = train_dataset.shuffle(buffer_size=1000, seed=42)
+    small_train_dataset = train_dataset.take(max(num_train,1))
+
+    # 2-2. 검증 데이터 줄이기
+    num_val = int(loader.val_size * subset_ratio)
+    val_dataset = val_dataset.shuffle(buffer_size=1000, seed=42)
+    small_val_dataset = val_dataset.take(max(num_val,1))
+
+    # 3. 모델 설정
+    model = get_model(max_len=sequence_length, dropout_step=0, dim=UMAP_OUTPUT_DIM, num_classes=NUM_CLASSES)
+    model.compile(
+        optimizer = keras.optimizers.AdamW(learning_rate=learning_rate, weight_decay=weight_decay),
+        loss = keras.losses.SparseCategoricalCrossentropy(),
+        metrics = ['accuracy']
+    )
+    print("Model compile finished.")
+
+    # 4. 모델 학습
+    history = model.fit(
+        small_train_dataset,
+        validation_data=small_val_dataset,
+        epochs=num_train_epochs,
+        verbose=1,
+        callbacks=[
+            keras.callbacks.ModelCheckpoint(
+                LOCAL_PATHS["gm_ckpt"],
+                monitor='val_loss',
+                save_best_only=True,
+                save_weights_only=False,
+                verbose=1
+            ),
+            keras.callbacks.EarlyStopping(
+                monitor='val_loss',
+                patience=10,
+                restore_best_weights=True,
+                verbose=1
+            ),
+            keras.callbacks.ReduceLROnPlateau(
+                monitor='val_loss',
+                factor=0.5,
+                patience=5,
+                min_lr=1e-6,
+                verbose=1
+            )
+        ]
+    )
+
+    # 5. 모델 평가
+    eval_result = model.evaluate(small_val_dataset, return_dict=True)
+
+    # 정확도(accuracy) 반환
+    return eval_result['accuracy']
+
+if __name__=="__main__":
+    # 1. 사용 가능한 GPU 리스트 출력
+    gpus = tf.config.list_physical_devices('GPU')
+
+    if not gpus:
+        print("WARNING: 지금 GPU가 아니라 CPU를 쓰고 있습니다!")
+    else:
+        # 현재 인식된 GPU 개수와 상세 명칭 출력
+        print(f"GPU 사용 중. 현재 사용 가능한 GPU 개수: {len(gpus)}")
+        for i, gpu in enumerate(gpus):
+            print(f" - GPU [{i}]: {gpu}")
+
+    """
+    TO-DO
+    transformer에서 다른 모델들로 확장
+    """
+    def get_optuna_config():
+        import argparse
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "-m", "--model",
+            default=OPTUNA_MODEL
+        )
+        parser.add_argument(
+            "-n", "--name", "--study_name",
+            default=OPTUNA_STUDY_NAME
+        )
+        # 데이터 사용 비율 설정
+        parser.add_argument(
+            "--sr", "--subset_ratio",
+            type=float,
+            default=SUBSET_RATIO
+        )
+        # 탐색 횟수
+        parser.add_argument(
+            "--nt", "--n_trials",
+            type=int,
+            default=N_TRIALS
+        )
+        args, _ = parser.parse_known_args()
+        print(*(f"   > {'[default]' if v==parser.get_default(k) else ''} {k}: {v} selected." for k,v in vars(args).items()), sep='\n')
+        return args
+    args = get_optuna_config()
+    # 사용자 옵션
+    study_name = args.name
+    subset_ratio = args.sr
+    n_trials = args.nt
+
+    # optuna 세팅
+    storage = RDBStorage(url=OPTUNA_TRIALS_PATH)
+    study = optuna.create_study(
+        study_name=study_name,
+        direction="maximize",
+        storage=storage,
+        load_if_exists=True
+    )
+
+    # 데이터 로드
+    loader = TrainDataLoader(data_path=LOAD_DATA, is_training_transformer=True)
+
+    print(f" ============== parameter trials {n_trials}번 시도 시작! ============== ")
+    study.optimize(objective, n_trials=n_trials)
+
+    print(f"Best value: {study.best_value}")
+    print(f"Best params: {study.best_params}")
+
+    # 최적 파라미터 추출
+    import json, datetime
+    date_idx = datetime.datetime.now().strftime("%Y_%m_%d_%H-%M")
+    with open(BEST_PARAMS_PATH, "w") as f:
+        json.dump(study.best_params, f)
--- train/gloss_transformer_train.py
+++ train/gloss_transformer_train.py
@@ -5,87 +5,95 @@ from pathlib import Path
 import wandb
 from wandb.integration.keras import WandbMetricsLogger
 import keras
+keras.mixed_precision.set_global_policy("mixed_float16") # fp16 가속 keras3 버전
 
 # 커스텀
 from src.backbone import get_model, TFLiteModel
 from src.config import MAX_LEN, LEARNING_RATE, EPOCHS, BATCH_SIZE, OUTPUT_DIM, NUM_CLASSES, \
-    WANDB_GM_PROJECT, WANDB_GM_NAME, L_CKPT, LOCAL_PATHS, LOAD_GM, LOAD_DATA
+    WANDB_GM_PROJECT, WANDB_GM_NAME, LOCAL_PATHS, LOAD_GM, LOAD_DATA, WEIGHT_DECAY, UMAP_OUTPUT_DIM
 from load_data.create_dataset import TrainDataLoader
 from load_data.create_dataset import upload_file
 
-def train_model(data_path: str):
+def train_model(data_path: str,
+                learning_rate: float=LEARNING_RATE, epochs: int=EPOCHS, batch_size: int=BATCH_SIZE, weight_decay: float=WEIGHT_DECAY,
+                max_sequence_len: int=MAX_LEN
+                ):
+    # print(" ============== 받은 하이퍼파라미터 ============== ")
+    # print(f"    > learning_rate: {learning_rate}")
+    # print(f"    > epochs: {epochs}")
+    # print(f"    > batch_size: {batch_size}")
+    # print(f"    > weight_decay: {weight_decay}")
+    # print(f"    > max_sequence_len: {max_sequence_len}")
+
     wandb.init(
         project=WANDB_GM_PROJECT,
         name=WANDB_GM_NAME,
         config={
-            "learning_rate": LEARNING_RATE,
-            "epochs": EPOCHS,
-            "batch_size": BATCH_SIZE
+            "learning_rate": learning_rate,
+            "epochs": epochs,
+            "batch_size": batch_size,
+            "max_sequence_len": max_sequence_len
         }
     )
     print("\nLoading training data...")
-    train_dataset, val_dataset = TrainDataLoader(data_path=data_path, is_training_transformer=True).create_transformer_dataset()
+    train_dataset, val_dataset, test_dataset = TrainDataLoader(data_path=data_path, max_len=max_sequence_len, is_training_transformer=True).create_transformer_dataset(batch_size=batch_size)
 
-    # Create model
+    # 1. 모델 생성
     print("\nCreating model...")
-    model = get_model(max_len=MAX_LEN, dropout_step=0, dim=OUTPUT_DIM, num_classes=NUM_CLASSES)
-
-    optimizer = keras.optimizers.Adam(learning_rate=LEARNING_RATE)
-
-    loss = keras.losses.SparseCategoricalCrossentropy()
-    metrics = ['accuracy']
-
-    model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+    model = get_model(max_len=max_sequence_len, dropout_step=0, dim=OUTPUT_DIM, num_classes=NUM_CLASSES)
+    model.compile(
+        optimizer=keras.optimizers.AdamW(learning_rate=learning_rate, weight_decay=weight_decay),
+        loss=keras.losses.SparseCategoricalCrossentropy(),
+        metrics=['accuracy']
+    )
 
     print(f"Model compiled successfully!")
     print(f"Input shape: {model.input_shape}")
     print(f"Output shape: {model.output_shape}")
 
-    # Callbacks
-    callbacks = [
-        keras.callbacks.ModelCheckpoint(
-            L_CKPT,
-            monitor='val_loss',
-            save_best_only=True,
-            save_weights_only=False,
-            verbose=1
-        ),
-        keras.callbacks.EarlyStopping(
-            monitor='val_loss',
-            patience=10,
-            restore_best_weights=True,
-            verbose=1
-        ),
-        keras.callbacks.ReduceLROnPlateau(
-            monitor='val_loss',
-            factor=0.5,
-            patience=5,
-            min_lr=1e-6,
-            verbose=1
-        ),
-        WandbMetricsLogger()
-    ]
-
-    # Train model
+    # 2. 모델 학습
     print("\nStarting training...")
     history = model.fit(
         train_dataset,
-        epochs=EPOCHS,
         validation_data=val_dataset,
-        callbacks=callbacks,
-        verbose=1
+        epochs=epochs,
+        verbose=1,
+        callbacks=[
+            keras.callbacks.ModelCheckpoint(
+                LOCAL_PATHS["gm_ckpt"],
+                monitor='val_loss',
+                save_best_only=True,
+                save_weights_only=False,
+                verbose=1
+            ),
+            keras.callbacks.EarlyStopping(
+                monitor='val_loss',
+                patience=10,
+                restore_best_weights=True,
+                verbose=1
+            ),
+            keras.callbacks.ReduceLROnPlateau(
+                monitor='val_loss',
+                factor=0.5,
+                patience=5,
+                min_lr=1e-6,
+                verbose=1
+            ),
+            WandbMetricsLogger()
+        ]
     )
 
+    # 3. 모델 저장
     print("\nSaving model...")
     model.save(LOCAL_PATHS["gm_final"])
     upload_file(local_root_path=str(Path(LOCAL_PATHS["gm_final"]).parent), upload_path=LOAD_GM, file_name=str(Path(Path(LOCAL_PATHS["gm_final"]).name)))
 
-    # Convert to TFLite
+    # 4. 경량화 모델 변환
     print("Converting to TFLite...")
     tflite_model = TFLiteModel(model)  # Pass single model, not list
 
     concrete_input_signature = tf.TensorSpec(
-        shape=[1, MAX_LEN, OUTPUT_DIM],  # (배치=1, 최대프레임=137, 채널=유맵차원)
+        shape=[1, max_sequence_len, UMAP_OUTPUT_DIM],  # (배치=1, 최대프레임=max_sequence_len, 채널=umap_dimension)
         dtype=tf.float32
     )
     concrete_function = tflite_model.__call__.get_concrete_function(concrete_input_signature)
@@ -95,7 +103,7 @@ def train_model(data_path: str):
 
     try:
         tflite_quant_model = converter.convert()
-        # Save TFLite model
+        # 7. 경량화 모델 저장
         with open(LOCAL_PATHS["gm_tflite"], 'wb') as f:
             f.write(tflite_quant_model)
         print("TFLite model saved successfully!")
@@ -104,10 +112,69 @@ def train_model(data_path: str):
         print(f"Warning: TFLite conversion failed: {e}")
 
     print("Training completed!")
+
+    # 5. 모델 평가
+    eval_results = model.evaluate(test_dataset, return_dict=True)
+    print("Model Test Results: ")
+    print(*(f"  > {k}: {v:.3f}" for k, v in eval_results.items()), sep='\n')
+
+    wandb.log({f"test_{k}": v for k, v in eval_results.items()})
     wandb.finish()
 
     return history
 
+def get_model_args():
+    """
+    명령어 예시: python -m train.gloss_transformer_train --storage L --lr 0.4 --bs 64 --epochs 120 --wd 0.03 --msl 170
+    """
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--lr", "--learning_rate",
+        type=float,
+        default=LEARNING_RATE,
+    )
+    parser.add_argument(
+        "--bs", "--batch_size",
+        type=int,
+        default=BATCH_SIZE
+    )
+    parser.add_argument(
+        "-e", "--epochs",
+        type=int,
+        default=EPOCHS
+    )
+    parser.add_argument(
+        "--wd", "--weight_decay",
+        type=float,
+        default=WEIGHT_DECAY
+    )
+    parser.add_argument(
+        "--msl", "--max_sequence_len",
+        type=int,
+        default=MAX_LEN
+    )
+
+    args, _ = parser.parse_known_args()
+    print(*(f"   > {'[default]' if v==parser.get_default(k) else ''} {k}: {v} selected." for k,v in vars(args).items()), sep='\n')
+    return args
+
 if __name__ == "__main__":
-    # Train model
-    history = train_model(data_path=LOAD_DATA)
+    # 1. 사용 가능한 GPU 리스트 출력
+    gpus = tf.config.list_physical_devices('GPU')
+
+    if not gpus:
+        print("WARNING: 지금 GPU가 아니라 CPU를 쓰고 있습니다!")
+    else:
+        # 현재 인식된 GPU 개수와 상세 명칭 출력
+        print(f"GPU 사용 중. 현재 사용 가능한 GPU 개수: {len(gpus)}")
+        for i, gpu in enumerate(gpus):
+            print(f" - GPU [{i}]: {gpu}")
+    # 2. 사용자 옵션 받기
+    args = get_model_args()
+
+    # 3. 모델 학습
+    history = train_model(data_path=LOAD_DATA,
+                          # 사용자 옵션 사용
+                          learning_rate=args.lr, batch_size=args.bs, epochs=args.epochs, weight_decay=args.wd, max_sequence_len=args.msl)
